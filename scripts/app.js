0caefacd0225cf889e0257d5f0745fe02a9dd263
93amrkakashi <93amr.kakashi@gmail.com>
2023-03-20 22:49:11 +0700

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+const burgerMenu = document.querySelector(".bar");
+const links = document.querySelector(".nav-links");
+
+
+console.log(links)
+
+
+function toggle() {
+   console.log("first")
+   links.classList.toggle("hide")
+}
+
+
+burgerMenu.addEventListener("click" , toggle)

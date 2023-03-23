b71c208c0645d18ed45ab8f4d78c5c05049e779f
93amrkakashi <93amr.kakashi@gmail.com>
2023-03-23 04:23:48 +0200

    section 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,11 @@
 const burgerMenu = document.querySelector(".bar");
 const links = document.querySelector(".nav-links");
 
-
-console.log(links)
-
+console.log(links);
 
 function toggle() {
-   console.log("first")
-   links.classList.toggle("hide")
+  console.log("first");
+  links.classList.toggle("hide");
 }
 
-
-burgerMenu.addEventListener("click" , toggle)
+burgerMenu.addEventListener("click", toggle);
